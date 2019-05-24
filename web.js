21e4a2babca9bf58832a144ce3b3e94a80e1c148
Chanhee <chanheeis@naver.com>
2019-05-25 07:08:26 +0900

    test

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var multer=require('multer');
 
 var storage=multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null,'views/public/image/uploads')
+        cb(null,'./views/public/image/uploads')
     },
     filename: function(req,file,cb){
         cb(null,file.originalname);
